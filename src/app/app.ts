bf8c8088f09b1e634daed65278e9f2e6d2837b7a
mauriicioreyes <mauricioreyes.dev@gmail.com>
2025-10-18 20:50:09 -0600

    Modifica lista con nueva sintaxis de Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
