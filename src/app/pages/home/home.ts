ed9633ae340e83d14fdc650f22f4833088ccea49
mauriicioreyes <mauricioreyes.dev@gmail.com>
2025-10-19 17:22:54 -0600

    Agrega la actualización de las tareas con programación inmutable

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,9 +2,11 @@ import { Component, signal } from '@angular/core';
 
 import { Task } from './../../models/task.model';
 
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
@@ -40,5 +42,19 @@ export class Home {
 
   deleteTask(index: number) {
     this.tasks.update( (tasks) => tasks.filter( (task, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if (position === index) {
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    });
   }
 }
